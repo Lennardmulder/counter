e7fb4405eaa80ec798a2aed065269a7a2b0ed58c
Lennardmulder <lennardmulder1991@gmail.com>
2020-04-15 21:48:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('https://localbitcoins.com/api/fees/')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+      console.log(this);
+    })
+    .catch(console.log)
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+            Blockchain vs the State
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
