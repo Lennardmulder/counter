e856081abd7d9cbe1fddcd1d87fc07d1ee0987bd
Lennardmulder <lennardmulder1991@gmail.com>
2020-04-15 23:24:31 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
+import rq from 'request-promise';
 
 import './App.css';
 
 class App extends React.Component {
+  
+
 
   componentDidMount() {
-    fetch('https://localbitcoins.com/api/fees/')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ contacts: data })
-      console.log(this);
-    })
-    .catch(console.log)
+    const rp = require('request-promise');
+    const requestOptions = {
+      method: 'GET',
+      uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+      qs: {
+        'start': '1',
+        'limit': '5000',
+        'convert': 'USD'
+      },
+      headers: {
+        'X-CMC_PRO_API_KEY': 'e8fc6284-adce-42cf-992c-19aadb498ff5'
+      },
+      json: true,
+      gzip: true
+    };
+    
+    rp(requestOptions).then(response => {
+      console.log('API call response:', response);
+    // }).then((data))  
+      console.log(response.data[100].name);
+    }).catch((err) => {
+      console.log('API call error:', err.message);
+    });
   }
 
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-            Blockchain vs the State
-        </header>
+      <div>
+        Hello
       </div>
     );
   }
